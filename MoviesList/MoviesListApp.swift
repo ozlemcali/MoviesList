3475d02b12f9629f12ef70e46bd624e0a16579e4
Ozlem Cali <ozlemmcali@gmail.com>
2022-11-03 20:09:00 +0300

    Initial Commit

---
+++ MoviesList/MoviesListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesListApp.swift
+//  MoviesList
+//
+//  Created by ozlem on 3.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct MoviesListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
